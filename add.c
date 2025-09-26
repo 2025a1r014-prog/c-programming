2b4796c2dd1b2f54de918ccd2d1a99e5fa88759e
2025a1r014-prog <2025a1r014@mietjammu.in>
2025-09-26 15:11:47 +0530

    Update add.c
    26sept

--- add.c
+++ add.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main(){
+    int x,y,z;
+    printf("first number");
+    scanf("%d",&x);
+    printf("second number");
+    scanf("%d",&y);
+    z=x+y;
+    printf("%d",z);
+    return 0;
+}
