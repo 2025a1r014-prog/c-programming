58379aaa4d34db237edb078a79758ad26893d85c
2025a1r014-prog <2025a1r014@mietjammu.in>
2025-09-29 10:52:13 +0530

    Create iflargest.c

---
+++ iflargest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter first number:");
+    scanf("%d",&a);
+    printf("enter second number");
+    scanf("%d",&a);
+    if(a<b)
+    printf("b is largest");
+    else
+    printf("a is largest");
+    return 0;
+}
