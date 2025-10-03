134f5158858f9813bb333d6373f5c4be9f8cdc1f
2025a1r014-prog <2025a1r014@mietjammu.in>
2025-10-03 15:51:00 +0530

    Update 1,4,9...n.c

--- 1,4,9....c
+++ 1,4,9....c
@@ -3,7 +3,7 @@ int main(){
     int i,n;
     printf("enter the value of n");
     scanf("%d",&n);
-    for(i=1;i<=n;i=i+1){
+    for(i=1;i<=n;i++){
         printf("%d\n",i*i);
     }
     return 0;
