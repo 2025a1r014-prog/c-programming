b5ec7673cd6492f04211a81a45eaa96eb30f9103
2025a1r014-prog <2025a1r014@mietjammu.in>
2025-10-03 15:53:56 +0530

    Create fton.c

---
+++ fton.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i,n,f;
+    printf("enter the value of f");
+    scanf("%d",&f);
+    printf("enter the value of n");
+    scanf("%d",&n);
+    for(i=f;i<=n;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
